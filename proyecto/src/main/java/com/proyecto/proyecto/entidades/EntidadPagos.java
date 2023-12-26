161e38ceecbfaf80b678811c6ab2420de222597e
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-26 11:28:33 -0500

    Update EntidadPagos.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPagos.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPagos.java
@@ -1,4 +1,19 @@
 package com.proyecto.proyecto.entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_pagos")
+@Data
 public class EntidadPagos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_pagos;
+
+    private Float monto;
+    private String fecha_pago;
+    private String metodo_pago;
+    private Boolean estado_pago;
 }
