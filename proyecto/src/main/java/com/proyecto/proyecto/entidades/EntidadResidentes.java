93682bd1f858a13a19f93db904ef4d23b2177cb4
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-26 11:24:30 -0500

    Update EntidadResidentes.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadResidentes.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadResidentes.java
@@ -1,7 +1,20 @@
 package com.proyecto.proyecto.entidades;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Table(name = "tbl_entidades")
+@Data
 public class EntidadResidentes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_residentes;
+    private String nombre,telefono, email;
+    private String fecha_ingreso;
+
+    //relacion de muchos a uno con unidades;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_unidad", referencedColumnName = "id_unidad")
+    private EntidadUnidades id_unidad;
 }
