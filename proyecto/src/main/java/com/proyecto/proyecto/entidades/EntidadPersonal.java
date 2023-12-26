35b144c39e0610459474d18c9abac54f0e910178
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-25 19:53:13 -0500

    Update EntidadPersonal.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPersonal.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPersonal.java
@@ -1,12 +1,21 @@
 package com.proyecto.proyecto.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name ="tbl_personales")
 @Data
 public class EntidadPersonal {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_personal;
+    private String nombre, puesto, telefono;
+    private String email;
+    private String fecha_contratación;
+    //relacion de muchos a uno con propiedades
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private 
 }
