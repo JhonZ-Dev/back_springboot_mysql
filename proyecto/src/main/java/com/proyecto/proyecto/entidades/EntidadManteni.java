ecd6f8ca11bb5ba945d0bda1d455453c3202d37c
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-25 20:04:05 -0500

    Update EntidadManteni.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadManteni.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadManteni.java
@@ -1,4 +1,16 @@
 package com.proyecto.proyecto.entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_mantenimientos")
+@Data
 public class EntidadManteni {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_mantenimiento;
+
+    pr
 }
