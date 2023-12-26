999e3e84d378603d39919dc7436abdc4abda5df7
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-25 19:45:19 -0500

    Update EntidadPropiedad.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPropiedad.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPropiedad.java
@@ -1,4 +1,14 @@
 package com.proyecto.proyecto.entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tlb_entidades")
+@Data
 public class EntidadPropiedad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_propiedad;
+    
 }
