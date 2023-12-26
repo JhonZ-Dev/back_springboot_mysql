f1bea82bd65cd4f69be32ce10538bdcbf819f714
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-26 11:41:29 -0500

    Update PropiedadesRepo.java

--- proyecto/src/main/java/com/proyecto/proyecto/repositorio/PropiedadesRepo.java
+++ proyecto/src/main/java/com/proyecto/proyecto/repositorio/PropiedadesRepo.java
@@ -1,4 +1,9 @@
 package com.proyecto.proyecto.repositorio;
 
-public interface PropiedadesRepo {
+import com.proyecto.proyecto.entidades.EntidadPropiedad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public interface PropiedadesRepo extends JpaRepository<EntidadPropiedad, Long> {
 }
