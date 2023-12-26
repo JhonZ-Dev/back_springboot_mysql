9daa2414f7d39f82a52a7edaece2e8a0b3eb19eb
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-26 11:26:20 -0500

    Update EntidadUnidades.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadUnidades.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadUnidades.java
@@ -3,6 +3,8 @@ package com.proyecto.proyecto.entidades;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tbl_unidades")
 @Data
@@ -20,4 +22,8 @@ public class EntidadUnidades {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_propiedad", referencedColumnName = "id_propiedad")
     private EntidadPropiedad id_propiedad;
+
+    //relacion de uno a muchos con residentes
+    @OneToMany(mappedBy = "id_unidad", cascade = CascadeType.ALL)
+    private List<EntidadResidentes> entidadResidentes;
 }
