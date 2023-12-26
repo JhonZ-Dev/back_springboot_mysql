b932b6caf342dab4d10abe42c4ec8c7e571d1fc0
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-25 19:49:00 -0500

    Update EntidadPersonal.java

--- proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPersonal.java
+++ proyecto/src/main/java/com/proyecto/proyecto/entidades/EntidadPersonal.java
@@ -1,4 +1,12 @@
 package com.proyecto.proyecto.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name ="tbl_personales")
+@Data
 public class EntidadPersonal {
+    
 }
