4e5a1eac6c6810249d19e4985703b61b369c3534
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-03 16:57:30 -0500

    Update PersonalController.java

--- proyecto/src/main/java/com/proyecto/proyecto/controladores/PersonalController.java
+++ proyecto/src/main/java/com/proyecto/proyecto/controladores/PersonalController.java
@@ -1,4 +1,21 @@
 package com.proyecto.proyecto.controladores;
 
+import com.proyecto.proyecto.entidades.EntidadPersonal;
+import com.proyecto.proyecto.servicios.PersonalServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/personal/proyecto")
 public class PersonalController {
+
+    @Autowired
+    public PersonalServicio servicio;
+
+    @PostMapping("/guardar/{id_propiedad}")
+    public EntidadPersonal guardarEntidad (
+            @RequestBody EntidadPersonal entidadPersonal,
+            @PathVariable Long id_propiedad){
+        return servicio.guardar(entidadPersonal, id_propiedad);
+    }
 }
