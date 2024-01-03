95a334ddf18e6cfdebefe8bc3f7824e0f08cd013
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-03 16:37:24 -0500

    Update PersonalServicio.java

--- proyecto/src/main/java/com/proyecto/proyecto/servicios/PersonalServicio.java
+++ proyecto/src/main/java/com/proyecto/proyecto/servicios/PersonalServicio.java
@@ -1,7 +1,13 @@
 package com.proyecto.proyecto.servicios;
 
+import com.proyecto.proyecto.repositorio.PersonalesRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonalServicio {
+
+    @Autowired
+    public PersonalesRepo repo;
+    
 }
