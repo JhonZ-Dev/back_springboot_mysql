2878bfbaaa9d33f873507f50bd270c00bd60546c
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-03 16:40:58 -0500

    Update PersonalServicio.java

--- proyecto/src/main/java/com/proyecto/proyecto/servicios/PersonalServicio.java
+++ proyecto/src/main/java/com/proyecto/proyecto/servicios/PersonalServicio.java
@@ -1,17 +1,26 @@
 package com.proyecto.proyecto.servicios;
 
 import com.proyecto.proyecto.entidades.EntidadPersonal;
+import com.proyecto.proyecto.entidades.EntidadPropiedad;
 import com.proyecto.proyecto.repositorio.PersonalesRepo;
+import com.proyecto.proyecto.repositorio.PropiedadesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PersonalServicio {
 
     @Autowired
     public PersonalesRepo repo;
+    @Autowired
+    public PropiedadesRepo propiedadesRepo;
 
     public EntidadPersonal guardar(EntidadPersonal entidadPersonal, Long id_propiedad){
+
+        //obteer la propiedad por su id
+        Optional<EntidadPropiedad> propiedadOptional = propiedadesRepo.findById(id_propiedad);
         
     }
 }
