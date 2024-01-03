73c24634713fa80edee4f6c104f7b8b65230edb5
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-03 16:50:06 -0500

    Update PersonalServicio.java

--- proyecto/src/main/java/com/proyecto/proyecto/servicios/PersonalServicio.java
+++ proyecto/src/main/java/com/proyecto/proyecto/servicios/PersonalServicio.java
@@ -17,18 +17,21 @@ public class PersonalServicio {
     @Autowired
     public PropiedadesRepo propiedadesRepo;
 
-    public EntidadPersonal guardar(EntidadPersonal personal, Long id_propiedad){
-
-        //obteer la propiedad por su id
+    public EntidadPersonal guardar(EntidadPersonal personal, Long id_propiedad) {
+        // Obtener la propiedad por su ID
         Optional<EntidadPropiedad> propiedadOptional = propiedadesRepo.findById(id_propiedad);
-        //verificar si la propiedad existe
-        if(propiedadOptional.isPresent()){
+
+        // Verificar si la propiedad existe
+        if (propiedadOptional.isPresent()) {
             EntidadPropiedad propiedad = propiedadOptional.get();
-            //asignar la propiedad al personal
+            // Asignar la propiedad al personal
             personal.setId_propiedad(propiedad);
-            return repo.save(personal);
-        }else{
+            // Guardar el personal y propagar los cambios a la propiedad
+            EntidadPersonal entidadPersonalGuardado = repo.save(personal);
+            return entidadPersonalGuardado;
+        } else {
             throw new RuntimeException("No se encontró la propiedad con ID: " + id_propiedad);
         }
     }
+
 }
